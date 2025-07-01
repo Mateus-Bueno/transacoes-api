b346ea2a18f4f43ec502272210fe7e2f3dcd1ddf
Mateus Antonio Bueno <128940046+Mateus-Bueno@users.noreply.github.com>
2025-06-30 22:04:22 -0300

    correção properties

---
+++ src/main/java/com/ana/transacao/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.leticia.categorias.config; // Altere esse pacote conforme o seu projeto
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://categorias-api-production.up.railway.app");
+        server.setDescription("Servidor Railway HTTPS");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Categorias")
+                        .version("v1")
+                        .description("Documentação da API de Categorias"))
+                .servers(List.of(server));
+    }
+}
